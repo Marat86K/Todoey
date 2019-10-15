5f63cb68edc99ae833331578328b14ce8a770bb5
Marat Kilmukhametov <kmd@live.ru>
2019-10-15 20:48:53 +0300

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Марат Кильмухаметов on 15.10.2019.
+//  Copyright © 2019 Marat Kilmukhametov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
